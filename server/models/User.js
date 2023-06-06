beb2d9ab857c682bfbe4a2c0e023e91cd0a1da6b
AlexC3105 <126286847+AlexC3105@users.noreply.github.com>
2023-06-06 10:50:51 -0400

    updated models

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// import schema from Book.js
+// Import the book schema from Book.js
 const bookSchema = require('./Book');
 
 const userSchema = new Schema(
@@ -21,10 +21,8 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    // set savedBooks to be an array of data that adheres to the bookSchema
-    savedBooks: [bookSchema],
+    savedBooks: [bookSchema], // Set savedBooks to be an array of data that adheres to the bookSchema
   },
-  // set this to use virtual below
   {
     toJSON: {
       virtuals: true,
@@ -32,22 +30,21 @@ const userSchema = new Schema(
   }
 );
 
-// hash user password
+// Hash user password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
-
   next();
 });
 
-// custom method to compare and validate password for logging in
+// Custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+// Define virtual field `bookCount` to get the number of saved books
 userSchema.virtual('bookCount').get(function () {
   return this.savedBooks.length;
 });
