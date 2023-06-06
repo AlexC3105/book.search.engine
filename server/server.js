f769517aee59848dabd31d2326ae5f0455505c67
AlexC3105 <126286847+AlexC3105@users.noreply.github.com>
2023-06-06 10:44:50 -0400

    updated utils

--- server/server.js
+++ server/server.js
@@ -9,13 +9,23 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startServer = async () => {
+  try {
+    await db.once('open');
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+    process.exit(1); // Exit with failure
+  }
+};
+
+startServer();
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,38 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+const authMiddleware = (req, res, next) => {
+  // Allow token to be sent via req.query or headers
+  let token = req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+  if (req.headers.authorization) {
+    // Extract token from the "Bearer <token>" format
+    token = token.split(' ')[1];
+  }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
+  if (!token) {
+    return res.status(400).json({ message: 'You have no token!' });
+  }
 
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
     next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  } catch (error) {
+    console.log('Invalid token:', error.message);
+    return res.status(400).json({ message: 'Invalid token!' });
+  }
+};
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+const signToken = ({ username, email, _id }) => {
+  const payload = { username, email, _id };
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+};
+
+module.exports = {
+  authMiddleware,
+  signToken,
 };
